9eac50281a443f5a674f769c2a80dfa06c5088af
Kazuya Takei <myself@attakei.net>
2023-03-25 22:07:25 +0900

    docs: Use furo as documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -7,5 +7,5 @@ extensions = []
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = "alabaster"
+html_theme = "furo"
 html_static_path = ["_static"]
