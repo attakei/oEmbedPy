c6106be1feddb1806df261fc127c74e371cf7ec2
Kazuya Takei <myself@attakei.net>
2025-01-29 03:00:13 +0900

    fix: Mark deprecated

--- oembedpy/ext/sphinx.py
+++ oembedpy/ext/sphinx.py
@@ -1,3 +1,8 @@
 """Sphinx extension module."""
 
+from sphinx.util.logging import getLogger
+
 from ..adapter.sphinx import setup
+
+logger = getLogger(__name__)
+logger.warning(f"DEPRECATED: '{__name__}' is moved to 'oembedpy.adapter.sphinx'.")
