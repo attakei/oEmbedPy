112cbbaed373617c9e4473ba8bfb6a58836e5ffa
Kazuya Takei <myself@attakei.net>
2025-01-29 03:00:01 +0900

    ci: Split tests 'ext' and 'adapter'

--- tests/test_ext_sphinx.py
+++ tests/test_ext_sphinx.py
@@ -33,36 +33,3 @@ def test_build_cover_depracated(app: SphinxTestApp, status, warning):  # noqa
     )
     assert iframe is not None
     assert iframe["width"] == "1200"
-
-
-@pytest.mark.sphinx("html", testroot="default")
-def test_build(app: SphinxTestApp, status, warning):  # noqa
-    soup = soup_html(app, "index.html")
-    # YourTube iframe render with maxwidth,maxheight
-    iframe = soup.find(
-        "iframe", src="https://www.youtube.com/embed/Oyh8nuaLASA?feature=oembed"
-    )
-    assert iframe is not None
-    assert iframe["width"] == "1200"
-
-
-@pytest.mark.sphinx("html", testroot="with-fallback")
-def test_build_with_fallback(app: SphinxTestApp):  # noqa
-    soup = soup_html(app, "index.html")
-    link = soup.find(
-        "a",
-        href="https://www.reddit.com/r/Python/comments/vdopqj/sphinxrevealjs_html_presentation_builder_for/",
-    )
-    assert link is not None
-
-
-@pytest.mark.sphinx("html", testroot="default")
-def test_caches(app: SphinxTestApp):  # noqa
-    app.build()
-    assert len(app.env.get_domain("oembedpy.adapter.sphinx").caches) == 3  # type: ignore[attr-defined]
-
-
-@pytest.mark.sphinx("html", testroot="for-sphinx-cached")
-def test_use_caches(app: SphinxTestApp):  # noqa
-    app.build()
-    assert len(app.env.get_domain("oembedpy.adapter.sphinx").caches) == 1  # type: ignore[attr-defined]
